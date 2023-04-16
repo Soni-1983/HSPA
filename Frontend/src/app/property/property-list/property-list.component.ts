b9d7079c187f6ec1557956e8f6351eb0ed2d203b
Soni-1983 <mypersonalrepo@outlook.com>
2023-04-17 03:28:35 +0500

    Implemented  property-card-component and property-list-component including css and animaton when hovering over images

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class PropertyListComponent {
 
+  properties: Array<any> = [
+    {
+    "Id": 1,
+    "Type": "House",
+    "Price": 12000,
+    "Name": "Birla House"
+    },
+    {
+      "Id": 2,
+      "Type": "Villa",
+      "Price": 14000,
+      "Name": "Sameer Villa"
+    },
+    {
+      "Id": 3,
+      "Type": "Farm Hourse",
+      "Price": 5000,
+      "Name": "Soni's Farm House"
+    },
+    {
+      "Id": 4,
+      "Type": "Apartment",
+      "Price": 11000,
+      "Name": "Belle View Towers"
+    },
+    {
+      "Id": 5,
+      "Type": "Apartment",
+      "Price": 12000,
+      "Name": "Signature Towers"
+    },
+    {
+      "Id": 6,
+      "Type": "House",
+      "Price": 25000,
+      "Name": "Soni House"
+    },
+    {
+      "Id": 7,
+      "Type": "House",
+      "Price": 30000,
+      "Name": "Moolani House"
+    },
+    {
+      "Id": 8,
+      "Type": "House",
+      "Price": 35000,
+      "Name": "Dwarkani House"
+    }
+  ]
 }
